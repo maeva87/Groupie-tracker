4d66c886b8e8dc3ad081b45a0d39b24a0d328744
Kinsley SUEDILE COLLET <kinsley.suedile-collet@epitech.eu>
2026-01-06 11:10:12 +0100

    [ADD] I remplaced all files.

--- Projet/main.go
+++ Projet/main.go
@@ -1,11 +1,22 @@
 package main
 
-import {
+import (
 	"fmt"
-	"groupie"
-	"io/outil"
+	"io/ioutil"
 	"net/http"
+)
+
+type Artist struct {
+	Name    string
+	Members []string
 }
 
 func main() {
+	fmt.Println("Starting server...")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		data, _ := ioutil.ReadFile("templates/index.html")
+		w.Write(data)
+	})
+	http.ListenAndServe(":8080", nil)
 }
