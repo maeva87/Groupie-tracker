3c65f7ff9f6eea8739ab92497d970706960c89b9
maéva neveu <maeva.neveu@ynov.com>
2026-01-06 10:57:05 +0100

    ADD server

--- Projet/main.go
+++ Projet/main.go
@@ -1,11 +1,24 @@
 package main
 
-import {
+import (
 	"fmt"
-	"groupie"
-	"io/outil"
 	"net/http"
-}
+	"text/template"
+)
 
 func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	http.Handle("/image/", http.StripPrefix("/image/", http.FileServer(http.Dir("./image"))))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("./templates/index.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		tmpl.Execute(w, nil)
+	})
+
+	fmt.Println("Serveur lancé sur http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
