4fc7f1376519ab794ca8b601b871c24e1dd48539
Kinsley SUEDILE COLLET <kinsley.suedile-collet@epitech.eu>
2026-02-10 10:53:43 +0100

    [UPDATE] Server artist search endpoint (WIP / partially functional)

--- Projet/main.go
+++ Projet/main.go
@@ -4,8 +4,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 	"text/template"
 )
+
+type APIIndex struct {
+	Artists   string `json:"artists"`
+	Locations string `json:"locations"`
+	Dates     string `json:"dates"`
+	Relation  string `json:"relation"`
+}
 
 type Artist struct {
 	ID           int      `json:"id"`
@@ -14,6 +22,89 @@ type Artist struct {
 	Members      []string `json:"members"`
 	CreationDate int      `json:"creationDate"`
 	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
+}
+
+type LocationData struct {
+	Index []struct {
+		ID        int      `json:"id"`
+		Locations []string `json:"locations"`
+		Dates     string   `json:"dates"`
+	} `json:"index"`
+}
+
+type DateData struct {
+	Index []struct {
+		ID    int      `json:"id"`
+		Dates []string `json:"dates"`
+	} `json:"index"`
+}
+
+type RelationData struct {
+	Index []struct {
+		ID             int                 `json:"id"`
+		DatesLocations map[string][]string `json:"datesLocations"`
+	} `json:"index"`
+}
+
+const apiBase = "https://groupietrackers.herokuapp.com/api"
+
+func fetchArtists() ([]Artist, error) {
+	resp, err := http.Get(apiBase + "/artists")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var artists []Artist
+	if err := json.NewDecoder(resp.Body).Decode(&artists); err != nil {
+		return nil, err
+	}
+	return artists, nil
+}
+
+func fetchLocations() (*LocationData, error) {
+	resp, err := http.Get(apiBase + "/locations")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var data LocationData
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return nil, err
+	}
+	return &data, nil
+}
+
+func fetchDates() (*DateData, error) {
+	resp, err := http.Get(apiBase + "/dates")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var data DateData
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return nil, err
+	}
+	return &data, nil
+}
+
+func fetchRelations() (*RelationData, error) {
+	resp, err := http.Get(apiBase + "/relation")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var data RelationData
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return nil, err
+	}
+	return &data, nil
 }
 
 func main() {
@@ -30,8 +121,16 @@ func main() {
 	})
 
 	http.HandleFunc("/api/artists", func(w http.ResponseWriter, r *http.Request) {
+		artists, err := fetchArtists()
+		if err != nil {
+			http.Error(w, "Erreur lors de la récupération des artistes", http.StatusInternalServerError)
+			return
+		}
 		query := r.URL.Query().Get("search")
-		artists := getArtists(query)
+		if query != "" {
+			artists = filterArtists(artists, query)
+		}
+
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(artists)
 	})
@@ -40,10 +139,20 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
-func getArtists(search string) []Artist {
-	allArtists := []Artist{
-		{ID: 1, Name: "Queen", Members: []string{"Freddie Mercury", "Brian May"}, CreationDate: 1970},
-		{ID: 2, Name: "SOJA", Members: []string{"Jacob Hemphill"}, CreationDate: 1997},
+func filterArtists(artists []Artist, query string) []Artist {
+	var result []Artist
+	q := strings.ToLower(query)
+	for _, a := range artists {
+		if strings.Contains(strings.ToLower(a.Name), q) {
+			result = append(result, a)
+			continue
+		}
+		for _, m := range a.Members {
+			if strings.Contains(strings.ToLower(m), q) {
+				result = append(result, a)
+				break
+			}
+		}
 	}
-	return allArtists
+	return result
 }
